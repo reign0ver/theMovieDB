1231ff46849e2627baeef88a1ad6b9b1dbcdccd9
Andrés <andres.carrillo@globant.com>
2021-12-23 16:29:19 -0500

    Create NetworkConstants file with the baseURL, apiKey and baseImageURL

---
+++ theMovieDB/theMovieDB/NetworkConstants.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkConstants.swift
+//  theMovieDB
+//
+//  Created by Andres Enrique Carrillo Miranda on 23/12/21.
+//
+
+import Foundation
+
+struct NetworkConstants {
+    static let baseURL = "https://api.themoviedb.org/3"
+    static let apiKey = "4a7aa5d9cec4625df8e4ff64ee86288f"
+    static let imageURL = "https://image.tmdb.org/t/p/w500/"
+}
