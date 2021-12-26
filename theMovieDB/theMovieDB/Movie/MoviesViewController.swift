4b2ae1eb1439281db3758e33d05ec629aa17b00a
Andrés <andres.carrillo@globant.com>
2021-12-26 03:26:09 -0500

    Delelte Main.storyboard and default ViewController. Create MoviesViewController and set the entrypoint of the app programatically

---
+++ theMovieDB/theMovieDB/Movie/MoviesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MoviesViewController.swift
+//  theMovieDB
+//
+//  Created by Andres Enrique Carrillo Miranda on 26/12/21.
+//
+
+import UIKit
+
+final class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MoviesViewController launched!")
+    }
+}
