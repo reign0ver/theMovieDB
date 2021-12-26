f06646df1c9ce19d3dc4bfb5f8c65114f3fbe81c
Andrés <andres.carrillo@globant.com>
2021-12-26 11:34:30 -0500

    Add Movies TableViewDelegate and DataSource

--- theMovieDB/theMovieDB/Movie/MoviesViewController.swift
+++ theMovieDB/theMovieDB/Movie/MoviesViewController.swift
@@ -7,15 +7,29 @@
 
 import UIKit
 
-final class MoviesViewController: UIViewController {
+final class MoviesViewController: UITableViewController {
     
+    private let viewModel: MoviesViewModelType
     weak var coordinator: MovieCoordinator?
-
+    
+    init(_ viewModel: MoviesViewModelType = MoviesViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .init(named: "appWhite")
+        viewModel.getMovies()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         setupNavigationBar()
-        print("MoviesViewController launched!")
+        view.backgroundColor = .appWhite
     }
 }
 
@@ -23,5 +37,20 @@ private extension MoviesViewController {
     func setupNavigationBar() {
         navigationItem.title = "Movies"
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+// MARK: - TableView Delegate and DataSource
+extension MoviesViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3 // remove this magic number
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
---
+++ theMovieDB/theMovieDB/UIColor+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extension.swift
+//  theMovieDB
+//
+//  Created by Andres Enrique Carrillo Miranda on 26/12/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static let appWhite: UIColor = .init(named: "appWhite") ?? .white
+}
